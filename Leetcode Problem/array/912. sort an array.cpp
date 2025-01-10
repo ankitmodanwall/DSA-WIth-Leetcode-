bfa3a1f116f0369ff3dc560e9b46917e5fe2ce55
ankitmodanwall <ankitmodanwal001@gmail.com>
2025-01-10 11:27:20 +0530

    name update

--- Leetcode Problem/array/912. sort an array.cpp
+++ Leetcode Problem/array/912. sort an array.cpp
@@ -1,25 +1,29 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> sortArray(vector<int>& nums) {
+    vector<int> sortArray(vector<int> &nums)
+    {
         unordered_map<int, int> mp;
-        for (int &num : nums){
+        for (int &num : nums)
+        {
             mp[num]++;
-
         }
 
-        int minE = *min_element(begin(nums), end(nums) );
+        int minE = *min_element(begin(nums), end(nums));
         int maxE = *max_element(begin(nums), end(nums));
 
-  int i = 0;
+        int i = 0;
 
-  for (int num = minE; num <= maxE; num++){
-    while(mp[num] > 0){
-        nums[i] = num;
-        i++;
-        mp[num]--;
-    }
-  }
+        for (int num = minE; num <= maxE; num++)
+        {
+            while (mp[num] > 0)
+            {
+                nums[i] = num;
+                i++;
+                mp[num]--;
+            }
+        }
 
-return nums;
+        return nums;
     }
 };
