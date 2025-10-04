868b11bb030973685dde032442aef8603a5eee64
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-10-04 16:38:50 +0530

    Time: 21 ms (19.21%), Space: 47.6 MB (97.49%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,17 +1,35 @@
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-
         int m = matrix.length;
         int n = matrix[0].length;
+        int ans = -1;
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> 
-        Integer.compare(o2, o1));
-            for(int r = 0; r<m;++ r){
-            for(int c = 0; c<n; ++c){
-           maxHeap.offer(matrix[r][c]);
-           if(maxHeap.size()>k)maxHeap.poll();
+        // Min-heap storing {value, row, column}
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(o -> o[0]));
+
+        // Step 1: Insert first element of each row (up to k rows) into the heap
+        for (int row = 0; row < Math.min(m, k); ++row) {
+            minHeap.offer(new int[] { matrix[row][0], row, 0 });
+        }
+
+        // Step 2: Extract min k times
+        for (int i = 0; i < k; ++i) {
+            int[] top = minHeap.poll();
+            int val = top[0];
+            int row = top[1];
+            int col = top[2];
+
+            ans = val;
+
+            // If there's a next column in the same row, add it to the heap
+            if (col + 1 < n) {
+                minHeap.offer(new int[] { matrix[row][col + 1], row, col + 1 });
             }
-            }
-            return maxHeap.poll();
+        }
+
+        return ans;
     }
 }
