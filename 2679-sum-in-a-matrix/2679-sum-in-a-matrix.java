4d6ef0fd461175e23411841d5971826aaf709267
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-10-11 03:37:53 +0530

    Time: 14 ms (39.63%), Space: 70.2 MB (60.53%) - LeetHub

--- 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
@@ -1,21 +1,21 @@
 class Solution {
     public int matrixSum(int[][] nums) {
-        
-          for(int[] row:nums){
-            Arrays.sort(row);
-          }
-          int score = 0;
-          for(int i = 0; i<nums[0].length; i++){
-            int max = nums[0][i];
-          for(int j = 0; j<nums.length;j++){
-            if(nums[j][i]>max){
-                max = nums[j][i];
-            }
-          }
-          score += max;
-          }
 
-return score;
+        for (int[] row : nums) {
+            Arrays.sort(row);
+        }
+        int score = 0;
+        for (int i = 0; i < nums[0].length; i++) {
+            int max = nums[0][i];
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j][i] > max) {
+                    max = nums[j][i];
+                }
+            }
+            score += max;
+        }
+
+        return score;
 
     }
 }
